456302c9ab340c290cebc4070e03ff7e58c1cdfa
Alexey Golubev <golubev.alexey.04@gmail.com>
2023-12-12 16:49:38 +0100

    Typing and matching functions implemented. Accuracy, timer and words counters are implemented

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+std::vector<std::string> words;
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    loadWords();
+    setRandomWord();
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +23,77 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::increaseWordCounter(){
+    wordCounter++;
+    ui->label_wordCounter->setText(QString::number(wordCounter));
+}
+
+void MainWindow::countAccuracy(const int typedLetters, const int correctLetters){
+    int accuracy = correctLetters * 100 / typedLetters;
+    ui->label_accuracy->setText(QString::number(accuracy) + "%");
+}
+
+void MainWindow::loadWords(){
+    std::ifstream inputFile("/Users/alexeygolubev/Documents/programming/TypingSpeedTest/words.txt");
+
+    if (!inputFile.is_open()) {
+        qDebug() << "File is not oppened!";
+        return;
+    }
+
+    std::string word;
+    while (inputFile >> word) {
+        if (word[0] == '#') {
+            continue;
+        }
+        words.push_back(word);
+    }
+
+    inputFile.close();
+}
+
+void MainWindow::setRandomWord()
+{
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+    if (!words.empty()) {
+        int randomIndex = std::rand() % words.size();
+        std::string randomWord = words[randomIndex];
+        ui->label->setText(QString::fromStdString(randomWord));
+    } else {
+        qDebug() << "No loaded words!";
+    }
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    static QString enteredWord = "";
+    static int cursorPosition = 0;
+    static QString necessaryWord = ui->label->text();
+
+    QString keyText = event->text();
+
+    increaseTypedLettersCounter();
+
+    if(necessaryWord.at(cursorPosition) == keyText){
+        enteredWord = enteredWord + keyText;
+        ui->label_2->setText(enteredWord);
+        cursorPosition++;
+        increaseCorrectLettersCounter();
+    }else{
+        qDebug() << "Incorrect letter";
+    }
+
+    countAccuracy(getTypedLettersCounter(), getCorrectLettersCounter());
+
+    if(cursorPosition >= necessaryWord.length()){
+        setRandomWord();
+        increaseWordCounter();
+        cursorPosition = 0;
+        necessaryWord = ui->label->text();
+        enteredWord = "";
+        ui->label_2->setText(enteredWord);
+    }
+
+//    QWidget::keyPressEvent(event);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QKeyEvent>
+#include <QDebug>
+#include <QLabel>
+#include <QTextDocument>
+#include <QTextCursor>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +20,41 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    int getWordCounter() const{
+        return wordCounter;
+    }
+
+    void increaseWordCounter();
+
+    void countAccuracy(const int typedLetters, const int correctLetters);
+
+    void increaseTypedLettersCounter(){
+        typedLettersCounter++;
+    }
+
+    int getTypedLettersCounter() const{
+        return typedLettersCounter;
+    }
+
+    void increaseCorrectLettersCounter(){
+        correctLettersCounter++;
+    }
+
+    int getCorrectLettersCounter() const{
+        return correctLettersCounter;
+    }
+
 private:
     Ui::MainWindow *ui;
+    int wordCounter = 0;
+    int typedLettersCounter = 0;
+    int correctLettersCounter = 0;
+
+protected:
+    void keyPressEvent(QKeyEvent *event) override;
+
+    void loadWords();
+
+    void setRandomWord();
 };
 #endif // MAINWINDOW_H
